ff9bdb94d5b4b736d4d796bcc1be24693f03398c
Ricardo Maurizio Paul <memaurifu14@hotmail.com>
2022-05-07 14:22:16 +0200

    ➕ use argh instead of clap

--- ceres/src/main.rs
+++ ceres/src/main.rs
@@ -1,70 +1,47 @@
 mod audio;
 mod emulator;
 
-use {
-    ceres_core::Model,
-    clap::{Arg, Command},
-    emulator::Emulator,
-    std::path::Path,
-};
+use {argh::FromArgs, ceres_core::Model, emulator::Emulator, std::path::Path};
 
 pub const CERES_STR: &str = "Ceres";
 const DMG_BOOT_ROM_PATH: &str = concat!("bootroms/bin/", "dmg_boot.bin");
 const MGB_BOOT_ROM_PATH: &str = concat!("bootroms/bin/", "mgb_boot.bin");
 const CGB_BOOT_ROM_PATH: &str = concat!("bootroms/bin/", "cgb_boot_fast.bin");
 
+#[derive(FromArgs)]
+#[argh(description = "cli arguments")]
+struct Args {
+    #[argh(positional, description = "rom to emulate")]
+    rom: String,
+
+    #[argh(option, description = "bootrom to use")]
+    bootrom: Option<String>,
+
+    #[argh(option, description = "model to emulate [dmg, mgb, cgb]")]
+    model: Option<String>,
+}
+
 fn main() {
-    let matches = Command::new(CERES_STR)
-        .about("GameBoy/Color emulator")
-        .arg(
-            Arg::new("rom")
-                .value_name("ROM")
-                .help("Cartridge ROM to emulate")
-                .takes_value(true)
-                .required(true),
-        )
-        .arg(
-            Arg::new("model")
-                .short('m')
-                .long("model")
-                .help("GameBoy model to emulate")
-                .possible_values(["dmg", "mgb", "cgb"])
-                .takes_value(true)
-                .ignore_case(true),
-        )
-        .arg(
-            Arg::new("boot")
-                .short('b')
-                .long("boot")
-                .help("Boot ROM to emulate")
-                .takes_value(true),
-        )
-        .get_matches();
+    let opts: Args = argh::from_env();
 
-    let model = if let Some(model_str) = matches.value_of("model") {
-        match model_str {
-            "dmg" => Model::Dmg,
-            "mgb" => Model::Mgb,
-            "cgb" => Model::Cgb,
-            _ => unreachable!("invalid model"),
+    let model = opts.model.map_or(Model::Cgb, |s| match s.as_str() {
+        "dmg" => Model::Dmg,
+        "mgb" => Model::Mgb,
+        "cgb" => Model::Cgb,
+        _ => panic!("invalid model"),
+    });
+
+    let boot_rom_str = opts.bootrom.unwrap_or_else(|| {
+        match model {
+            Model::Dmg => DMG_BOOT_ROM_PATH,
+            Model::Mgb => MGB_BOOT_ROM_PATH,
+            Model::Cgb => CGB_BOOT_ROM_PATH,
         }
-    } else {
-        Model::Cgb
-    };
-
-    let boot_rom_str = matches.value_of("boot").unwrap_or(match model {
-        Model::Dmg => DMG_BOOT_ROM_PATH,
-        Model::Mgb => MGB_BOOT_ROM_PATH,
-        Model::Cgb => CGB_BOOT_ROM_PATH,
+        .to_owned()
     });
 
     let boot_rom_path = Path::new(&boot_rom_str);
-
-    let rom_path = matches
-        .value_of("rom")
-        .map(|s| Path::new(s).to_path_buf())
-        .unwrap();
-
+    let rom_path = Path::new(&opts.rom).to_path_buf();
     let emulator = Emulator::new(model, boot_rom_path, &rom_path);
 
     emulator.run();
