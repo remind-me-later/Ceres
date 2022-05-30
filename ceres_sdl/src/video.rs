7e42ae689601a8ea9dff803e5587913bae32ef08
Ricardo Maurizio Paul <memaurifu14@hotmail.com>
2022-05-30 22:04:01 +0200

    🎨 opengl cleanup

--- ceres_sdl/src/video.rs
+++ ceres_sdl/src/video.rs
@@ -1,11 +1,16 @@
 use {
-    core::{ffi::c_void, mem, ptr},
     gl::types::*,
     sdl2::{
         video::{GLContext, Window},
         Sdl, VideoSubsystem,
     },
-    std::time::Instant,
+    std::{
+        cmp::min,
+        ffi::{c_void, CString},
+        mem::size_of,
+        ptr,
+        time::Instant,
+    },
 };
 
 const INDICES: [GLubyte; 6] = [
@@ -25,36 +30,36 @@ const PX_WIDTH: u32 = ceres_core::PX_WIDTH as u32;
 const PX_HEIGHT: u32 = ceres_core::PX_HEIGHT as u32;
 const MUL: u32 = 4;
 
-pub trait Context {
-    fn get_proc_address(&mut self, procname: &str) -> *const c_void;
-    fn swap_buffers(&mut self);
-    fn make_current(&mut self);
-}
-
 pub struct Renderer {
     window: Window,
     next_frame: Instant,
     vbo: GLuint,
     vao: GLuint,
     ebo: GLuint,
-    program: Program,
+    program: GlProgram,
     texture: GLuint,
-    transform_location: GLint,
+    transform_loc: GLint,
     // keep them safe and cozy
-    _video_subsystem: VideoSubsystem,
+    _video: VideoSubsystem,
     _context: GLContext,
 }
 
 impl Renderer {
     pub fn new(sdl: &Sdl) -> Renderer {
         unsafe {
-            let video_subsystem = sdl.video().unwrap();
+            let video = sdl.video().unwrap();
 
-            let gl_attr = video_subsystem.gl_attr();
+            let gl_attr = video.gl_attr();
             gl_attr.set_context_profile(sdl2::video::GLProfile::Core);
             gl_attr.set_context_version(3, 3);
+            gl_attr.set_depth_size(0);
+            gl_attr.set_context_flags().forward_compatible().set();
 
-            let mut window = video_subsystem
+            if !cfg!(debug_assertions) {
+                gl_attr.set_context_no_error(true);
+            }
+
+            let mut window = video
                 .window(crate::CERES_STR, PX_WIDTH * MUL, PX_HEIGHT * MUL)
                 .opengl()
                 .position_centered()
@@ -67,45 +72,39 @@ impl Renderer {
             let context = window.gl_create_context().unwrap();
             window.gl_make_current(&context).unwrap();
 
-            gl::load_with(|s| video_subsystem.gl_get_proc_address(s).cast());
+            gl::load_with(|s| video.gl_get_proc_address(s).cast());
 
-            let program = {
-                let vert_shader =
-                    Shader::from_src(include_bytes!("shader/vs.vert"), gl::VERTEX_SHADER);
-                let frag_shader =
-                    Shader::from_src(include_bytes!("shader/fs.frag"), gl::FRAGMENT_SHADER);
-                Program::new(vert_shader, frag_shader)
-            };
+            let program = GlProgram::new(
+                include_str!("shader/vs.vert"),
+                include_str!("shader/fs.frag"),
+            );
 
-            let mut vbo = 0;
             let mut vao = 0;
-            let mut ebo = 0;
-
             gl::GenVertexArrays(1, &mut vao);
-            gl::GenBuffers(1, &mut vbo);
-            gl::GenBuffers(1, &mut ebo);
-
             gl::BindVertexArray(vao);
 
+            let mut vbo = 0;
+            gl::GenBuffers(1, &mut vbo);
             gl::BindBuffer(gl::ARRAY_BUFFER, vbo);
             gl::BufferData(
                 gl::ARRAY_BUFFER,
-                (VERTICES.len() * mem::size_of::<GLfloat>()) as GLsizeiptr,
-                mem::transmute(&VERTICES[0]),
+                (VERTICES.len() * size_of::<GLfloat>()) as GLsizeiptr,
+                VERTICES.as_ptr().cast(),
                 gl::STATIC_DRAW,
             );
 
+            let mut ebo = 0;
+            gl::GenBuffers(1, &mut ebo);
             gl::BindBuffer(gl::ELEMENT_ARRAY_BUFFER, ebo);
             gl::BufferData(
                 gl::ELEMENT_ARRAY_BUFFER,
-                (INDICES.len() * mem::size_of::<GLubyte>()) as GLsizeiptr,
-                mem::transmute(&INDICES[0]),
+                (INDICES.len() * size_of::<GLubyte>()) as GLsizeiptr,
+                INDICES.as_ptr().cast(),
                 gl::STATIC_DRAW,
             );
 
-            let stride = (4 * mem::size_of::<GLfloat>()) as GLsizei;
-
             // position attribute
+            let stride = (4 * size_of::<GLfloat>()) as GLsizei;
             gl::VertexAttribPointer(0, 2, gl::FLOAT, gl::FALSE, stride, ptr::null());
             gl::EnableVertexAttribArray(0);
 
@@ -116,7 +115,7 @@ impl Renderer {
                 gl::FLOAT,
                 gl::FALSE,
                 stride,
-                (2 * mem::size_of::<GLfloat>()) as *const c_void,
+                (2 * size_of::<GLfloat>()) as *const c_void,
             );
             gl::EnableVertexAttribArray(1);
 
@@ -129,9 +128,8 @@ impl Renderer {
             gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_MIN_FILTER, gl::NEAREST as GLint);
             gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_MAG_FILTER, gl::NEAREST as GLint);
 
-            // create transform
-            let transform_name = b"transform\0";
-            let transform_location = program.get_uniform_location(transform_name.as_ptr().cast());
+            // get transform pointer
+            let transform_loc = program.get_uniform_location(b"transform\0".as_ptr().cast());
 
             let mut video_renderer = Renderer {
                 window,
@@ -140,9 +138,9 @@ impl Renderer {
                 ebo,
                 program,
                 texture,
-                transform_location,
+                transform_loc,
                 next_frame: Instant::now(),
-                _video_subsystem: video_subsystem,
+                _video: video,
                 _context: context,
             };
 
@@ -153,24 +151,25 @@ impl Renderer {
     }
 
     pub fn resize_viewport(&mut self, width: u32, height: u32) {
-        let gb_width = ceres_core::PX_WIDTH as u32;
-        let gb_height = ceres_core::PX_HEIGHT as u32;
-        let multiplier = core::cmp::min(width / gb_width, height / gb_height);
-        let surface_width = gb_width * multiplier;
-        let surface_height = gb_height * multiplier;
+        let multiplier = min(width / PX_WIDTH, height / PX_HEIGHT);
+        let surface_width = PX_WIDTH * multiplier;
+        let surface_height = PX_HEIGHT * multiplier;
 
         let x = surface_width as f32 / width as f32;
         let y = surface_height as f32 / height as f32;
 
         // hand-written scale matrix
-        let transform = [
-            x, 0.0, 0.0, 0.0, 0.0, y, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0,
+        let t = [
+            x, 0.0, 0.0, 0.0, //
+            0.0, y, 0.0, 0.0, //
+            0.0, 0.0, 1.0, 0.0, //
+            0.0, 0.0, 0.0, 1.0,
         ];
 
         unsafe {
-            self.program.use_program();
             gl::Viewport(0, 0, width as i32, height as i32);
-            gl::UniformMatrix4fv(self.transform_location, 1, gl::FALSE, transform.as_ptr());
+            self.program.bind();
+            gl::UniformMatrix4fv(self.transform_loc, 1, gl::FALSE, t.as_ptr());
         }
     }
 
@@ -190,19 +189,18 @@ impl Renderer {
         );
     }
 
-    pub fn draw_frame(&mut self, rgba_pixel_data: &[u8]) {
+    pub fn draw_frame(&mut self, rgba: &[u8]) {
         unsafe {
-            self.update_texture(rgba_pixel_data);
+            self.update_texture(rgba);
 
             let now = Instant::now();
             if now < self.next_frame {
                 std::thread::sleep(self.next_frame - now);
             }
 
-            gl::BindTexture(gl::TEXTURE_2D, self.texture);
             gl::ClearColor(0.0, 0.0, 0.0, 1.0);
             gl::Clear(gl::COLOR_BUFFER_BIT);
-            self.program.use_program();
+            self.program.bind();
             gl::BindVertexArray(self.vao);
             gl::DrawElements(gl::TRIANGLES, 6, gl::UNSIGNED_BYTE, ptr::null());
 
@@ -223,61 +221,20 @@ impl Drop for Renderer {
     }
 }
 
-struct Shader {
+struct GlProgram {
     id: GLuint,
+    vert_shader: GLuint,
+    frag_shader: GLuint,
 }
 
-impl Shader {
-    pub unsafe fn from_src(source: &'static [u8], shader_type: GLenum) -> Self {
-        let id = gl::CreateShader(shader_type);
-        // Attempt to compile the shader
-        let mut c_string = source.to_vec();
-        c_string.push(b'\0');
-        gl::ShaderSource(id, 1, &(c_string.as_ptr().cast()), ptr::null());
-        gl::CompileShader(id);
-
-        // Get the compile status
-        let mut status = gl::FALSE as GLint;
-        gl::GetShaderiv(id, gl::COMPILE_STATUS, &mut status);
-
-        if status != gl::TRUE as GLint {
-            let mut len = 0;
-            gl::GetShaderiv(id, gl::INFO_LOG_LENGTH, &mut len);
-            let mut buf = Vec::with_capacity(len as usize);
-            gl::GetShaderInfoLog(id, len, ptr::null_mut(), buf.as_mut_ptr() as *mut GLchar);
-            buf.pop(); // ignore '/0'
-
-            let msg = core::str::from_utf8(&buf)
-                .unwrap_or("ShaderInfoLog not valid utf8")
-                .to_owned();
-
-            panic!("{msg}");
-        }
-
-        Self { id }
-    }
-}
-
-impl Drop for Shader {
-    fn drop(&mut self) {
-        unsafe {
-            gl::DeleteShader(self.id);
-        }
-    }
-}
-
-struct Program {
-    id: GLuint,
-    // keep them alive
-    _vert_shader: Shader,
-    _frag_shader: Shader,
-}
-
-impl Program {
-    pub unsafe fn new(vert_shader: Shader, frag_shader: Shader) -> Self {
+impl GlProgram {
+    pub unsafe fn new(vert_src: &str, frag_src: &str) -> Self {
         let id = gl::CreateProgram();
-        gl::AttachShader(id, vert_shader.id);
-        gl::AttachShader(id, frag_shader.id);
+        let vert_shader = Self::shader_from_src(vert_src, gl::VERTEX_SHADER);
+        let frag_shader = Self::shader_from_src(frag_src, gl::FRAGMENT_SHADER);
+
+        gl::AttachShader(id, vert_shader);
+        gl::AttachShader(id, frag_shader);
         gl::LinkProgram(id);
         // Get the link status
         let mut status = gl::FALSE as GLint;
@@ -299,12 +256,40 @@ impl Program {
 
         Self {
             id,
-            _vert_shader: vert_shader,
-            _frag_shader: frag_shader,
+            vert_shader,
+            frag_shader,
         }
     }
 
-    pub unsafe fn use_program(&self) {
+    unsafe fn shader_from_src(src: &str, shader_type: GLenum) -> GLuint {
+        let id = gl::CreateShader(shader_type);
+        // Attempt to compile the shader
+        let c_string = CString::new(src).unwrap();
+        gl::ShaderSource(id, 1, &(c_string.as_ptr().cast()), ptr::null());
+        gl::CompileShader(id);
+
+        // Get the compile status
+        let mut status = gl::FALSE as GLint;
+        gl::GetShaderiv(id, gl::COMPILE_STATUS, &mut status);
+
+        if status != gl::TRUE as GLint {
+            let mut len = 0;
+            gl::GetShaderiv(id, gl::INFO_LOG_LENGTH, &mut len);
+            let mut buf = Vec::with_capacity(len as usize);
+            gl::GetShaderInfoLog(id, len, ptr::null_mut(), buf.as_mut_ptr() as *mut GLchar);
+            buf.pop(); // ignore '/0'
+
+            let msg = core::str::from_utf8(&buf)
+                .unwrap_or("ShaderInfoLog not valid utf8")
+                .to_owned();
+
+            panic!("{msg}");
+        }
+
+        id
+    }
+
+    pub unsafe fn bind(&self) {
         gl::UseProgram(self.id);
     }
 
@@ -313,10 +298,12 @@ impl Program {
     }
 }
 
-impl Drop for Program {
+impl Drop for GlProgram {
     fn drop(&mut self) {
         unsafe {
             gl::DeleteProgram(self.id);
+            gl::DeleteShader(self.vert_shader);
+            gl::DeleteShader(self.frag_shader);
         }
     }
 }
