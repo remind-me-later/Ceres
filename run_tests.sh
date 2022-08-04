82c276237ff15399d24978d09c0124a808afe5f5
DalmeGNU <dalmemail@gmail.com>
2022-08-04 12:54:22 +0200

    Implement testing framework

---
+++ run_tests.sh
@@ -0,0 +1,15 @@
+cd ceres_test
+FILES="../tests/*"
+for f in $FILES
+do
+	cp $f test.asm
+	rgbasm test.asm -o obj.o
+	rgblink obj.o -o obj.gb
+	rgbfix -v -p 0 obj.gb
+	echo -n "Running $f... "
+	cargo run --quiet obj.gb
+done
+rm test.asm
+rm obj.o
+rm obj.gb
+cd ..
